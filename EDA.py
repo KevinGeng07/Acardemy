5e8317a090585f3d3e3fe9bc0b3c869b0fb73f1e
Kevin Geng <kevingeng@berkeley.edu>
2025-11-15 14:24:29 -0800

    Tabular EDA for college_results.

--- EDA.py
+++ EDA.py
@@ -1,3 +1,47 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from tabulate import tabulate
+
+ag_csv = pd.read_csv('Affordability Gap Data AY2022-23 2.17.25.xlsx - Affordability_latest_02-17-25 1.csv')
+
+cr_csv = pd.read_csv('College Results View 2021 Data Dump for Export.xlsx - College Results View 2021 Data .csv')
+# print(cr_csv.columns)
+
+### Replace 'NA' with np.nan.
+cr_csv.replace('NA', np.nan, inplace=True)
+
+### All rows have some null values.
+print(len(cr_csv), cr_csv.isna().any(axis=1).sum())
+
+### Count number of nulls. -> Remove columns with 65%+ total nulls & rows with 65%+ total nulls.
+print(cr_csv.isna().sum())
+# print(cr_csv.columns[cr_csv.isna().mean() > 0.75])
+cr_csv = cr_csv[cr_csv.columns[cr_csv.isna().mean() <= 0.65]]
+cr_csv = cr_csv[cr_csv.isna().mean(axis=1) <= 0.65]
+
+print(len(cr_csv.columns), len(cr_csv))
+
+
+writefile = 'college_results_table.txt'
+open(writefile, 'w').close()
+
+with open(writefile, 'a') as wf:
+    feature_table = [['Feature', '25th', 'Median', '75th', 'Mean', 'Std']]
+
+    for col in cr_csv.columns:
+        feature_row = [col]
+
+        cr_feature = cr_csv[col]
+        cr_feature = cr_feature.dropna().reset_index(drop=True)
+
+        if cr_feature.dtype == object: continue
+
+        feature_row.extend([np.percentile(cr_feature, 25), np.percentile(cr_feature, 50), np.percentile(cr_feature, 75),
+                           np.mean(cr_feature), np.std(cr_feature)])
+
+        feature_table.append(feature_row)
+
+    
+    wf.write(tabulate(feature_table[1:], headers=feature_table[0], tablefmt='fancy_grid'))
+    wf.close()
