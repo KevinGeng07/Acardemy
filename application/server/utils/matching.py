53064e6d6abd5539891a42b6ee9f6c57b22c7e76
Hachiman <141888124+Hachiman-potassiumdesu@users.noreply.github.com>
2025-11-16 12:53:52 -0800

    fix matching

--- application/server/utils/matching.py
+++ application/server/utils/matching.py
@@ -5,16 +5,6 @@ def l2_distance(a, b):
 
     return distance
 
-# def cosine_similarity(a, b):
-#     dot = np.dot(a, b)
-#     norm_a = np.linalg.norm(a)
-#     norm_b = np.linalg.norm(b)
-
-#     return dot / (norm_a * norm_b)
-
-# def l2_distance(a, b):
-#     distance = 
-
 std = 0.75
 
 def similarities(expectation, actual):
